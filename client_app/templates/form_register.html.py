d68a81bc4afc5b3c712c419458c244b42e7d414f
Tim Nelke <tim.nelke@outlook.com>
2019-12-19 13:44:04 +0100

    forms now are toggled via the new toggleFunction

--- client_app/templates/form_register.html.py
+++ client_app/templates/form_register.html.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1576758294.0044515
+_modified_time = 1576758976.2516713
 _enable_loop = True
 _template_filename = 'C:/Development/vsy_pax-counter/client_app/templates/form_register.html'
 _template_uri = 'form_register.html'
@@ -18,7 +18,7 @@ def render_body(context,**pageargs):
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         __M_writer = context.writer()
-        __M_writer('<form class="login">\r\n    <h1>Register</h1>\r\n    <div class="input"><input type="text" placeholder="E-Mail" name="email"></div>\r\n    <div class="input"><input type="password" placeholder="Password" name="password"></div>\r\n    <div class="input"><input type="password" placeholder="Repeat Password" name="passwordSafety"></div>\r\n    <div>\r\n        <input type="button" onclick="register()" value="Register">\r\n        <input type="button" class="link" onclick="toggleRegisterForm()" value="I already have an account">\r\n    </div>\r\n</form>')
+        __M_writer('<form class="login">\r\n    <h1>Register</h1>\r\n    <div class="input"><input type="text" placeholder="E-Mail" name="email"></div>\r\n    <div class="input"><input type="password" placeholder="Password" name="password"></div>\r\n    <div class="input"><input type="password" placeholder="Repeat Password" name="passwordSafety"></div>\r\n    <div>\r\n        <input type="button" onclick="register()" value="Register">\r\n        <input type="button" class="link" onclick="toggleForm()" value="I already have an account">\r\n    </div>\r\n</form>')
         return ''
     finally:
         context.caller_stack._pop_frame()
